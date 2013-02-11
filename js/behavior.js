40a71fd3aff716fc6710f6e8ea9c018f54929216
Paul DeLeeuw <pauld@teamddm.com>
2013-02-11 15:02:33 -0500

    Added triggering so that the webapp version would stay in-app

--- js/behavior.js
+++ js/behavior.js
@@ -1,3 +1,14 @@
 $('#infoModal').on('shown', function() {
 	$(".modal-footer a.install").attr('href', $('.modal-body a.applink').attr('href'));
 });
+
+if (window.navigator.standalone) {
+	var local = document.domain;
+	$('a').click(function() {
+		var a = $(this).attr('href');
+		if ( a.match('http://' + local) || a.match('http://www.' + local) ){
+			event.preventDefault();
+			document.location.href = a;
+		}
+	});
+}
